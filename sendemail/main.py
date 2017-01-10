9a2526a6e1188db7af35b80289c5c0a3e366ee39
gengzhibo <gengzhibo@gengzhibo.net>
2017-01-10 15:01:22 +0800

    20170110

---
+++ sendemail/main.py
@@ -0,0 +1,66 @@
+
+def main():
+    import smtplib
+    from email.mime.text import MIMEText
+    from email.mime.multipart import MIMEMultipart
+    from email.mime.image import MIMEImage
+
+    mail_host = 'smtp.163.com'
+    mail_user = 'm1***'
+    mail_pass = '******'
+    sender = 'm1***@163.com'
+    receivers = ['***@qq.com']
+
+
+    #只发送文本的时候
+    # message = MIMEText('conten1111t', 'plain', 'utf-8')
+    # message['Subject'] = 'title'
+    # message['From'] = sender
+    # message['To'] = receivers[0]
+
+
+    #需要发送附件的时候
+    message = MIMEMultipart()
+    message['From'] = sender
+    message['To'] = receivers[0]
+    message['Subject'] = 'title'
+
+    #通过html修饰文本格式
+    # with open('abc.html', 'r') as f:
+    #     content = f.read()
+    # html = MIMEText(content, 'html', 'utf-8')
+
+    with open('abc.txt', 'r')as h:
+        content2 = h.read()
+    file = MIMEText(content2, 'plain', 'utf-8')
+    file['Content-Type'] = 'application/octet-stream'
+    file['Content-Disposition'] = 'attachment;filename="abc.txt"'
+
+
+    #图片附件
+    with open('1.png', 'rb')as fp:
+        picture = MIMEImage(fp.read())
+        picture['Content-Type'] = 'application/octet-stream'
+        picture['Content-Disposition'] = 'attachment;filename="1.png"'
+
+
+    # 将内容附加到邮件主体中
+    # message.attach(html)
+    message.attach(file)
+    message.attach(picture)
+
+
+
+    # 登录并发送邮件
+    try:
+        smtpObj = smtplib.SMTP()
+        smtpObj.connect(mail_host, 25)
+        smtpObj.login(mail_user, mail_pass)
+        smtpObj.sendmail(sender, receivers, message.as_string())
+        smtpObj.quit()
+        print('send success')
+    except smtplib.SMTPException as e:
+        print('error', e)
+
+if __name__ == '__main__':
+    main()
