61052cb4f0c9fdc720af91b1a5e1a4beae6a5180
gengzhibo <gengzhibo@gengzhibo.net>
2017-01-10 19:12:27 +0800

    20170110

---
+++ AnaMap/anamap_vi/anamap_vi/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/haizhi/Desktop/python_uitls/AnaMap/anamap_vi/anamap_vi/bin/python3.5
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
---
+++ AnaMap/main.py
@@ -0,0 +1,75 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-'
+
+import matplotlib
+from matplotlib.dates import datestr2num
+from matplotlib.font_manager import FontProperties
+
+matplotlib.use('TkAgg')
+import tushare as ts
+import matplotlib.pyplot as plt
+
+myfont = FontProperties(fname='/Library/Fonts/huawenfangsong.ttf')
+matplotlib.rcParams['axes.unicode_minus']=False
+
+
+
+def main():
+    data = ts.get_hist_data('sz50', start='2016-11-01', end='2016-12-30')
+    data = data.sort_index()
+
+
+    # # 一个基本的折线图
+    x = range(len(data))
+    # 收盘价的折线图
+    plt.plot(x, data['close'])
+    plt.title(u'中文' , fontproperties=myfont)
+    #
+    # x = range(len(data))
+    # plt.plot(x, data['close'])
+    # plt.plot(x, data['high'])
+    #
+    # # 设置图形大小和线条颜色
+    # x = range(len(data))
+    # plt.figure(figsize=(10, 5))
+    # plt.plot(x, data['close'])
+    # plt.plot(x, data['high'])
+
+    # x = range(len(data))
+    # plt.figure(figsize=(10, 5))
+    # plt.plot(x, data['close'])
+    # plt.plot(x, data['high'], color='r')
+    # plt.title("中文")
+
+    # from matplotlib.pylab import datestr2num
+    # x = range(len(data))
+    # x_date = [datestr2num(i) for i in data.index]
+    # plt.figure(figsize=(10, 5))
+    # plt.title("上证50指数历史最高价、收盘价走势折线图")
+    # plt.xlabel("时间")
+    # # plt.xticks(x,[i for i in data.index])
+    # plt.ylabel("指数")
+    # plt.plot_date(x_date, data['close'], '-', label="收盘价")
+    # plt.plot_date(x_date, data['high'], '-', color='r', label="开盘价")
+    # plt.legend()
+
+
+    # x = range(len(data))
+    # x_date = [datestr2num(i) for i in data.index]
+    # plt.style.use('ggplot')
+    # plt.figure(figsize=(10, 5))
+    # plt.title("image")
+    # plt.xlabel("time")
+    # plt.xticks(rotation=45)
+    # plt.ylabel("index")
+    # plt.plot_date(x_date, data['close'], '-', label="sp")
+    # plt.plot_date(x_date, data['high'], '-', label="max")
+    # plt.legend()
+    # plt.grid(True)
+
+
+    plt.show()
+
+if __name__ == '__main__':
+    main()
+
