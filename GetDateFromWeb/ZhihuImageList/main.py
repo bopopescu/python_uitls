0b4c17877c7bdc542c939135eeed0de0ed9ce1d8
clwater <gengzhibo@gengzhibo.net>
2017-05-02 23:57:45 +0800

    20170502

--- GetDateFromWeb/ZhihuImageList/main.py
+++ GetDateFromWeb/ZhihuImageList/main.py
@@ -1,8 +1,8 @@
+import os
 
 import requests
 import re
 import json
-
 
 
 def findIsEnd(html):
@@ -14,38 +14,79 @@ def findIsEnd(html):
     return is_end
 
 
-def main(questionid):
+def saveimage(imagesrc, relimage):
+    parentname = imagesrc
+    parentname = re.findall('com/.*?_r\.(?:jpg|png)' , parentname)
+    parentname = parentname[0]
+    parentname = parentname.replace('com/' , '')
+    parentname = parentname.replace('_r', '')
+
+    filename = 'image/' + relimage + '/' + parentname
+
+    # print(filename)
+
+    pd = os.path.isfile(filename)
+
+    if pd:
+        print(relimage + 'is create')
+    else:
+        try:
+            r = requests.get(imagesrc)
+            with open(filename, 'wb') as f:
+                for chunk in r.iter_content(chunk_size=1024):
+                    f.write(chunk)
+        except Exception :
+            print(filename + 'crate error')
+
+
+def createFile(filename):
+    filename = 'image/' + filename
+    try:
+        os.mkdir(filename)
+    except Exception :
+        print('文件夹创建失败')
+
+
+
+def main(questionid ):
+    headers = {'authorization': 'oauth c3cef7c66a1843f8b3a9e6a1e3160e20',
+               'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.81 Safari/537.36'}
+
+    _url = 'https://www.zhihu.com/api/v4/questions/%s'%(questionid)
+    titleReponse = requests.get(_url , headers = headers)
+    titleHtml = titleReponse.text
+
+    titleJson = json.loads(titleHtml)
+    title = titleJson['title']
+    title = str(title)
+    title = title.replace('？' , '')
+    # print(title)
+
 
     index = 0
     _isEnd = False
-    #
-    # url = 'https://www.zhihu.com/api/v4/questions/%s/answers?limit=20&offset='%(questionid)
-    # print(url)
-
-    headers = {'authorization': 'oauth c3cef7c66a1843f8b3a9e6a1e3160e20' , 'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.81 Safari/537.36'}
 
 
+
+    createFile(title)
 
     while not _isEnd:
-        # url = 'https://www.zhihu.com/api/v4/questions/%s/answers?sort_by=default&include=data%5B%2A%5D.is_normal%2Cis_sticky%2Ccollapsed_by%2Csuggest_edit%2Ccomment_count%2Ccan_comment%2Ccontent%2Ceditable_content%2Cvoteup_count%2Creshipment_settings%2Ccomment_permission%2Cmark_infos%2Ccreated_time%2Cupdated_time%2Crelationship.is_authorized%2Cis_author%2Cvoting%2Cis_thanked%2Cis_nothelp%2Cupvoted_followees%3Bdata%5B%2A%5D.author.badge%5B%3F%28type%3Dbest_answerer%29%5D.topics&limit=20&offset=%s'%(questionid , index)
-
-        # url = 'https://www.zhihu.com/api/v4/questions/%s/answers?limit=20&offset=%s'%(questionid , index)
-
         url = 'https://www.zhihu.com/api/v4/questions/%s/answers?include=data[*].is_normal,content&limit=20&offset=%s'%(questionid , index)
 
-        # print(url)
+        print(url)
 
         index = index + 20
 
         reponse = requests.get(url , headers=headers)
         html = reponse.text
 
-        print(html)
+        # print(html)
 
         alljson = json.loads(html)
         # print(alljson['data'])
 
         alldata = alljson['data']
+
 
         for data in alldata:
 
@@ -54,14 +95,25 @@ def main(questionid):
 
             content = json.loads(pageJson)
             text = content['content']
-            print( text )
+            # print( text )
 
+            _index = True
+            imageList = re.findall('data-original=\"https?://pic.*?(?:png|jpg|jpeg)', text)
+            for image in imageList:
+                if _index:
+                    image = image.replace('data-original=\"' , '')
+                    print(image)
+                    saveimage(image , title)
+                _index = not _index
+
+
+        # _isEnd = True
         _isEnd = findIsEnd(html)
 
 
 
 
-main(46312145)
+main(46312145 )
 
 
 
